340ad4d1ebf344154857344b4164ffa3f0ad8e45
noemesa93 <noemesa93@gmail.com>
2024-10-29 12:37:25 +0100

    tercercommit

--- src/controllers/users_controllers.js
+++ src/controllers/users_controllers.js
@@ -32,7 +32,7 @@ const loginUser = async (req, res, next) => {
         return res.status(401).json({ message: 'No existe el email y/o contraseña' })
     }
     res.json({
-        message: 'Login correcto',
+        message: '👌',
         token: createToken(user)
     })
 }
